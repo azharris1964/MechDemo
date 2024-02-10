196034936562ad65c9362685a2c9a99e40bc06d7
Antonio <turtleloser210@gmail.com>
2024-02-09 16:20:36 -0800

    plugins_2

---
+++ MechDemo/testBuild1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,60 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void Jetpack_Start_m6A401928435BB053532C7753B8E165FD5D952823 (void);
+extern void Jetpack_FixedUpdate_mFAE46620490E98FCA083932D006BE66144EBED8D (void);
+extern void Jetpack_OnEnable_m79FA4720ADE86AF6019EE3045B842FB0C5AF3BEF (void);
+extern void Jetpack_OnDisable_mDF585B21D0A09EC17A9873F9136CFAFA904F7A1F (void);
+extern void Jetpack_SetTriggerValue_m158D41E4719164C471F9277779FCB7A339E881C9 (void);
+extern void Jetpack__ctor_m9063D61B1EF6954AA9ECAC06F7DACE7F69C587B2 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	Jetpack_Start_m6A401928435BB053532C7753B8E165FD5D952823,
+	Jetpack_FixedUpdate_mFAE46620490E98FCA083932D006BE66144EBED8D,
+	Jetpack_OnEnable_m79FA4720ADE86AF6019EE3045B842FB0C5AF3BEF,
+	Jetpack_OnDisable_mDF585B21D0A09EC17A9873F9136CFAFA904F7A1F,
+	Jetpack_SetTriggerValue_m158D41E4719164C471F9277779FCB7A339E881C9,
+	Jetpack__ctor_m9063D61B1EF6954AA9ECAC06F7DACE7F69C587B2,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	7785,
+	7785,
+	7785,
+	7785,
+	6325,
+	7785,
+	12911,
+	7785,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
